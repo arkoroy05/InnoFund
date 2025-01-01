1025ffbb07c75926d24ded899001d53e26028ad3
arkoroy05 <132930851+arkoroy05@users.noreply.github.com>
2025-01-01 21:42:57 +0530

    auth working end to end

--- client/app/(routes)/feed/page.tsx
+++ client/app/(routes)/feed/page.tsx
@@ -1,8 +1,14 @@
-import React from 'react'
+"use client"
+import React, { useEffect } from 'react'
+import { useAuth, UserButton,useUser } from '@clerk/nextjs'
 
 const page = () => {
+  const {user}=useUser()
   return (
-    <div>page</div>
+    <div>
+      WASSUP {user?.firstName}
+      <UserButton />{/** this is usericon, it doesnt accept any props/classname */}
+    </div>
   )
 }
 
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,43 @@
+"use client"
+
 import { UserProfile } from '@clerk/clerk-react'
-import { SignInButton, SignOutButton, SignUpButton, UserButton } from '@clerk/nextjs'
-import React from 'react'
+import { SignInButton, SignOutButton, SignUpButton, UserButton, useAuth, useUser } from '@clerk/nextjs'
+import axios from 'axios'
+import { useRouter } from 'next/navigation'
+import React, { useEffect } from 'react'
 
 const page = () => {
+  const router = useRouter()
+  const { isSignedIn, userId } = useAuth()
+  const { user } = useUser()
+  
+
+  useEffect(() => {
+    const createUser = async () => {
+      if (user && user.username && user.emailAddresses[0]?.emailAddress  && user.primaryWeb3Wallet?.web3Wallet) {
+        const userData = {
+          name: user.username,
+          email: user.emailAddresses[0].emailAddress,
+          wallet: user.primaryWeb3Wallet?.web3Wallet
+        }
+        
+        try {
+          await axios.post("/api/users", userData)
+        } catch (error) {
+          console.log('Error creating user:', error)
+        }
+      }
+    }
+
+    if (user) {
+      createUser()
+    }
+
+    if (isSignedIn) {
+      router.push(`/feed`)
+    }
+  }, [user, isSignedIn, router])
+
   return (
     <div className="flex gap-4 items-center">
       <SignInButton>
@@ -10,9 +45,7 @@ const page = () => {
           Sign In
         </button>
       </SignInButton>
-      {/** this is how youll render the components, style the button, not sign in button */}
-     <UserButton />
-      
+      <UserButton />
       <SignUpButton>
         <button className="bg-green-500 hover:bg-green-600 text-white font-semibold py-2 px-4 rounded-lg transition-colors">
           Sign Up
