1ddb87f09c5f4d24d3297387661aab24806fa485
psycocodes <gmohikshit@gmail.com>
2025-01-04 13:38:56 +0530

    Changed Explore

--- client/app/(routes)/explore/page.tsx
+++ client/app/(routes)/explore/page.tsx
@@ -33,14 +33,12 @@ const ExplorePage = () => {
                     title={item.name}
                     field={item.field}
                     desc={item.desc || "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
-                    timeline={item.timeline}
+                    timeposted={item.timeline}
                     potentialImpact={item.potentialImpact}
                     teamSize={item.teamSize}
                     author={item.author}
                     currentFunding={item.currentFunding}
                     goalFunding={item.goalFunding}
-                    backers={item.backers}
-                    daysLeft={item.daysLeft}
                 />
             ))}
         </div>
