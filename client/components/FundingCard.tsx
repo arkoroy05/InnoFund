3b7aaaf6d4b235c2d42248035ce07c1443af68e0
psycocodes <gmohikshit@gmail.com>
2025-01-04 23:10:40 +0530

    Made Explore work

--- client/components/FundingCard.tsx
+++ client/components/FundingCard.tsx
@@ -23,6 +23,7 @@ import {
 } from "@/components/ui/tooltip";
 import { Clipboard, Check } from "lucide-react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 interface Author {
   username: string;
@@ -31,7 +32,8 @@ interface Author {
 }
 
 interface ResearchFundingCardProps {
-  key: number;
+  key: Number;
+  id: string;
   title: string;
   desc: string;
   field: string;
@@ -106,6 +108,7 @@ export function formatTimeRemaining(endDate: Date | null): string {
 }
 
 export default function FundingCard({
+  id,
   title,
   desc,
   field,
@@ -127,134 +130,134 @@ export default function FundingCard({
       ? "https://avatars.githubusercontent.com/u/0"
       : author?.photoURL || "https://avatars.githubusercontent.com/u/0",
   };
+  const router = useRouter();
   return (
     <>
       <TooltipProvider>
-        <Card
-          className="w-full rounded-[0] border-neutral-700/20 hover:bg-neutral-900 flex justify-between flex-col"
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <CardHeader className="">
-            <div className="flex items-center space-x-4 mb-2">
+        <Link href={`/projects/${id}`}>
+          <Card
+            className="w-full rounded-[0] border-neutral-700/20 hover:bg-neutral-900 flex justify-between flex-col"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          >
+            <CardHeader className="">
+              <div className="flex items-center space-x-4 mb-2">
+                <Tooltip>
+                  <TooltipTrigger>
+                    <Avatar>
+                      <AvatarImage
+                        src={
+                          user.photoURL ||
+                          `https://avatars.githubusercontent.com/${user.username}?size=200`
+                        }
+                        alt={`GitHub avatar for ${user.username}`}
+                      />
+                      <AvatarFallback>
+                        <AvatarImage src="/avalanche-avax-logo.svg" />
+                      </AvatarFallback>
+                    </Avatar>
+                  </TooltipTrigger>
+                  <TooltipContent className="bg-neutral-900 text-white border border-neutral-700">
+                    {"Avatar for " + user.username}
+                  </TooltipContent>
+                </Tooltip>
+                <div className="flex flex-col w-[calc(100%-4rem)]">
+                  <span className="text-neutral-700">{user.name}</span>
+                  <span className="text-neutral-500 truncate items-center w-[calc(100%-4rem)] text-sm">
+                    {user?.username}
+                  </span>
+                  <span className="text-neutral-500 text-sm">
+                    <Button
+                      variant="ghost"
+                      className="h-4 hover:bg-transparent p-0 my-1"
+                      onClick={() => {
+                        navigator.clipboard.writeText(user.username!);
+                        setIsCopied(true);
+                        setTimeout(() => setIsCopied(false), 5000);
+                      }}
+                      disabled={isCopied}
+                    >
+                      {isCopied ? (
+                        <>
+                          <Check className="size-4 text-green-500 " />
+                          Copied!
+                        </>
+                      ) : (
+                        <>
+                          <Clipboard className="size-4" />
+                          Copy UserID
+                        </>
+                      )}
+                    </Button>
+                  </span>
+                </div>
+                <div className="text-neutral-500 text-sm"></div>
+              </div>
               <Tooltip>
-                <TooltipTrigger>
-                  <Avatar>
-                    <AvatarImage
-                      src={
-                        user.photoURL ||
-                        `https://avatars.githubusercontent.com/${user.username}?size=200`
-                      }
-                      alt={`GitHub avatar for ${user.username}`}
-                    />
-                    <AvatarFallback>
-                      <AvatarImage src="/avalanche-avax-logo.svg" />
-                    </AvatarFallback>
-                  </Avatar>
-                </TooltipTrigger>
+                <a href={primaryLink} className="text-3xl truncate">
+                  <TooltipTrigger>
+                    <CardTitle>{title}</CardTitle>
+                  </TooltipTrigger>
+                </a>
                 <TooltipContent className="bg-neutral-900 text-white border border-neutral-700">
-                  {"Avatar for " + user.username}
+                  {primaryLink === "/explore"
+                    ? `${title}`
+                    : `${title}:${primaryLink}`}
                 </TooltipContent>
               </Tooltip>
-              <div className="flex flex-col w-[calc(100%-4rem)]">
-                <span className="text-neutral-700">{user.name}</span>
-                <span className="text-neutral-500 truncate items-center w-[calc(100%-4rem)] text-sm">
-                  {user?.username}
-                </span>
-                <span className="text-neutral-500 text-sm">
-                  <Button
-                    variant="ghost"
-                    className="h-4 hover:bg-transparent p-0 my-1"
-                    onClick={() => {
-                      navigator.clipboard.writeText(user.username!);
-                      setIsCopied(true);
-                      setTimeout(() => setIsCopied(false), 5000);
-                    }}
-                    disabled={isCopied}
-                  >
-                    {isCopied ? (
-                      <>
-                        <Check className="size-4 text-green-500 " />
-                        Copied!
-                      </>
-                    ) : (
-                      <>
-                        <Clipboard className="size-4" />
-                        Copy UserID
-                      </>
-                    )}
-                  </Button>
-                </span>
-              </div>
-              <div className="text-neutral-500 text-sm"></div>
-            </div>
-            <Tooltip>
-              <a href={primaryLink} className="text-3xl truncate">
-                <TooltipTrigger>
-                  <CardTitle>{title}</CardTitle>
+              <Tooltip>
+                <CardDescription className="font-medium truncate text-xl text-neutral-700">
+                  <TooltipTrigger>{field}</TooltipTrigger>
+                </CardDescription>
+                <TooltipContent className="bg-neutral-900 text-white border border-neutral-700">
+                  {field}
+                </TooltipContent>
+              </Tooltip>
+              <Tooltip>
+                <TooltipTrigger className="text-left">
+                  <CardDescription className="line-clamp-3 ">{desc}</CardDescription>
                 </TooltipTrigger>
-              </a>
-
-              <TooltipContent className="bg-neutral-900 text-white border border-neutral-700">
-                {primaryLink === "/explore"
-                  ? `${title}`
-                  : `${title}:${primaryLink}`}
-              </TooltipContent>
-            </Tooltip>
-            <Tooltip>
-              <CardDescription className="font-medium truncate text-xl text-neutral-700">
-                <TooltipTrigger>{field}</TooltipTrigger>
-              </CardDescription>
-              <TooltipContent className="bg-neutral-900 text-white border border-neutral-700">
-                {field}
-              </TooltipContent>
-            </Tooltip>
-
-            <Tooltip>
-              <TooltipTrigger className="text-left">
-                <CardDescription className="line-clamp-3 ">{desc}</CardDescription>
-              </TooltipTrigger>
-              <TooltipContent className="bg-neutral-900 text-white border border-neutral-700 max-w-[300px]">{desc}</TooltipContent>
-            </Tooltip>
-          </CardHeader>
-
-          <CardFooter className="flex items-center flex-col justify-between">
-            <span className="mb-2">
-              <span className="text-neutral-400 px-2 py-1 rounded-full bg-neutral-800">
-                {formatDistanceToNow(new Date(timeposted))}
-              </span>{" "}
-              {"\u2022"}{" "}
-              <span className="text-gray-300 border px-2 py-1 rounded-full bg-neutral-800">
-                {formatTimeRemaining(completionTime)}
+                <TooltipContent className="bg-neutral-900 text-white border border-neutral-700 max-w-[300px]">{desc}</TooltipContent>
+              </Tooltip>
+            </CardHeader>
+            <CardFooter className="flex items-center flex-col justify-between">
+              <span className="mb-2">
+                <span className="text-neutral-400 px-2 py-1 rounded-full bg-neutral-800">
+                  {formatDistanceToNow(new Date(timeposted))}
+                </span>{" "}
+                {"\u2022"}{" "}
+                <span className="text-gray-300 border px-2 py-1 rounded-full bg-neutral-800">
+                  {formatTimeRemaining(completionTime)}
+                </span>
               </span>
-            </span>
-            <div className="w-full">
-              <Progress value={progress} className="m-2"></Progress>
-              <div className="flex justify-between w-full">
-                <span className="text-neutral-600">
-                  {currentFunding.toLocaleString()} AVAX
-                </span>
-                <span className="text-lime-500">
-                  {goalFunding.toLocaleString()} AVAX
-                </span>
+              <div className="w-full">
+                <Progress value={progress} className="m-2"></Progress>
+                <div className="flex justify-between w-full">
+                  <span className="text-neutral-600">
+                    {currentFunding.toLocaleString()} AVAX
+                  </span>
+                  <span className="text-lime-500">
+                    {goalFunding.toLocaleString()} AVAX
+                  </span>
+                </div>
               </div>
-            </div>
-            <Tooltip>
-              <TooltipTrigger className="w-full">
-                <Button variant={"outline"} className="w-full mt-2 mb-1 py-6">
-                  <Image
-                    src="/avalanche-avax-logo.svg"
-                    alt="Avalanche Logo"
-                    width={15}
-                    height={15}
-                  />
-                  Donate with AVAX
-                </Button>
-              </TooltipTrigger>
-              <TooltipContent>Donate with AVAX</TooltipContent>
-            </Tooltip>
-          </CardFooter>
-        </Card>
+              <Tooltip>
+                <TooltipTrigger className="w-full">
+                  <Button variant={"outline"} className="w-full mt-2 mb-1 py-6">
+                    <Image
+                      src="/avalanche-avax-logo.svg"
+                      alt="Avalanche Logo"
+                      width={15}
+                      height={15}
+                    />
+                    Donate with AVAX
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>Donate with AVAX</TooltipContent>
+              </Tooltip>
+            </CardFooter>
+          </Card>
+        </Link>
       </TooltipProvider>
     </>
   );
