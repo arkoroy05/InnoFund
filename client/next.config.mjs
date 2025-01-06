da010e4f2332e293b43cce287cb99c5e677eedd4
psycocodes <gmohikshit@gmail.com>
2025-01-06 15:13:53 +0530

    updated nextjs config

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    typescript: {
+      // Ignores TypeScript errors during build
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      // Ignores ESLint errors during build
+      ignoreDuringBuilds: true,
+    },
+  };
 export default nextConfig;
